df1506ca25f187147495b85113125e1f82e7e8e5
danie1r <r.seunghyeondaniel@wustl.edu>
2023-01-22 11:23:53 -0600

    fc

---
+++ my-app/src/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+import UserLog from "./components/UserLog";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>CONCERTGO</h1>
+        <UserLog />
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ my-app/src/components/LocationSearch.jsx
@@ -0,0 +1,9 @@
+import { useState } from "react";
+import axios from "axios";
+
+function Location() {
+  const [results, setResults] = useState([]);
+
+}
+
+export default Location;
